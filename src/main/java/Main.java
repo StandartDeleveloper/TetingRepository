c10b3324807aab5e4b27589435cd966e2d0dfcc1
Makarenkov Kirill <workmakarenkovke@yandex.ru>
2018-09-28 23:31:48 +0300

    First Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,46 @@
+/**
+ * Created by OEM on 27.09.2018.
+ */
+import edu.princeton.cs.introcs.*;
+import edu.princeton.cs.introcs.Draw;
+import edu.princeton.cs.introcs.StdDraw;
+import edu.princeton.cs.introcs.Stopwatch;
+import javafx.scene.paint.Stop;
+
+public class Main {
+    public static void main(String[] args) {
+        Draw draw1 = new Draw("Test client 1");
+
+        for (double i = 0.1; i <4 ; i=i+0.25) {
+            for (double j = 0.1; j <4 ; j=j+0.25) {
+                draw1.square(j,i,0.1);
+            }
+        }
+
+//        draw1.square(0.1,0.1,0.1);
+//        draw1.square(0.35,0.1,0.1);
+
+
+//        while(true){
+//
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            StdDraw.setPenRadius(0.05);
+//            StdDraw.setPenColor(StdDraw.BLUE);
+//            StdDraw.point(Math.random(), Math.random());
+//
+//
+//            try {
+//                Thread.sleep(100);
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//            StdDraw.clear();
+//        }
+
+    }
+
+}
